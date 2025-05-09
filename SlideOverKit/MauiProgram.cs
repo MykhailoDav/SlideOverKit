6d46d2c0892aff9f2e91f10364418a727fb71cc6
Mykhailo Davydenko <davudenko.mukhailo@outlook.com>
2025-05-09 19:35:11 +0300

    cleanup

--- SlideOverKit/MauiProgram.cs
+++ SlideOverKit/MauiProgram.cs
@@ -14,7 +14,7 @@ public static class MauiProgram
         builder
             .UseMauiApp<App>()
 #if ANDROID
-            .UseMauiCompatibility()
+            .UseMauiCompatibility()            
 #endif
             .ConfigureFonts(fonts =>
             {
